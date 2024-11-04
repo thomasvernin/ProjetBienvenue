12e14929a4d156c5e974e772cd5dd264cdb8a763
Vernin Thomas <tvernin49100@gmail.com>
2024-11-04 14:32:03 +0100

    Initial commit

---
+++ src/Bienvenue.java
@@ -0,0 +1,14 @@
+public class Bienvenue {
+
+    public static void main(String[] args) {
+        // Affiche un message de bienvenue
+        System.out.println("Bienvenue dans mon premier projet Java avec Eclipse !");
+        
+        // Un petit calcul pour montrer un peu de logique
+        int a = 5;
+        int b = 10;
+        int somme = a + b;
+        
+        System.out.println("La somme de " + a + " et " + b + " est : " + somme);
+    }
+}
